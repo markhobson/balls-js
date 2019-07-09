5c880f303140562fde9343bc57bfb00619968f48
Mark Hobson <markhobson@gmail.com>
2019-07-09 19:39:42 +0100

    Extract World class out of App

--- App.js
+++ App.js
@@ -1,32 +1,12 @@
 (function (Ball) {
-	function App(canvas) {
+	function World(canvas, balls) {
 		var self = this;
 		self.context = canvas.getContext('2d');
-		self.balls = newBalls();
+		self.balls = balls;
 		self.run = run;
 		self.tick = tick;
 		self.plot = plot;
 		self.animate = animate;
-		
-		function newBalls() {
-			var balls = new Array(10);
-			for (var i = 0; i < balls.length; i++) {
-				balls[i] = newBall();
-			}
-			return balls;
-		}
-		
-		function newBall() {
-			var canvas = self.context.canvas;
-			var r = 25 + Math.random() * 25;
-			var x = r + Math.random() * (canvas.width - 2 * r);
-			var y = r + Math.random() * (canvas.height - 2 * r);
-			var dx = 10 * Math.random() - 5;
-			var dy = 10 * Math.random() - 5;
-			var color = '#' + parseInt(Math.random() * 0xFFFFFF).toString(16);
-			
-			return new Ball(x, y, r, dx, dy, color);
-		}
 		
 		function run() {
 			setInterval(this.tick.bind(this), 10);
@@ -50,6 +30,35 @@
 			this.balls.forEach(function (ball) {
 				ball.animate(this.context);	
 			}, this);
+		}
+	}
+	
+	function App(canvas) {
+		var self = this;
+		self.world = new World(canvas, newBalls(canvas));
+		self.run = run;
+		
+		function newBalls(canvas) {
+			var balls = new Array(10);
+			for (var i = 0; i < balls.length; i++) {
+				balls[i] = newBall(canvas);
+			}
+			return balls;
+		}
+		
+		function newBall(canvas) {
+			var r = 25 + Math.random() * 25;
+			var x = r + Math.random() * (canvas.width - 2 * r);
+			var y = r + Math.random() * (canvas.height - 2 * r);
+			var dx = 10 * Math.random() - 5;
+			var dy = 10 * Math.random() - 5;
+			var color = '#' + parseInt(Math.random() * 0xFFFFFF).toString(16);
+			
+			return new Ball(x, y, r, dx, dy, color);
+		}
+		
+		function run() {
+			this.world.run();
 		}
 	}
 	
