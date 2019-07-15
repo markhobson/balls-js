3cb35cb49bcb0a0f6aeccf42fa50648aef2c2ea7
Mark Hobson <markhobson@gmail.com>
2019-07-15 12:32:54 +0100

    Pass dimensions to init rather than canvas

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 (function (World, Ball) {
 	var canvas = document.getElementById('app');
-	var objects = randomBalls(10, canvas);
+	var objects = randomBalls(10, canvas.width, canvas.height);
 	new World(canvas, objects).run();
 
-	function randomBalls(n, canvas) {
+	function randomBalls(n, width, height) {
 		var balls = new Array(n);
 		for (var i = 0; i < balls.length; i++) {
-			balls[i] = randomNonIntersectingBall(canvas, balls);
+			balls[i] = randomNonIntersectingBall(width, height, balls);
 		}
 		return balls;
 	}
 	
-	function randomNonIntersectingBall(canvas, balls) {
+	function randomNonIntersectingBall(width, height, balls) {
 		var ball;
 		do {
-			ball = randomBall(canvas);
+			ball = randomBall(width, height);
 		}
 		while (intersecting(ball, balls));
 		return ball;
@@ -29,10 +29,10 @@
 		return false;
 	}
 
-	function randomBall(canvas) {
+	function randomBall(width, height) {
 		var radius = 25 + Math.random() * 25;
-		var x = radius + Math.random() * (canvas.width - 2 * radius);
-		var y = radius + Math.random() * (canvas.height - 2 * radius);
+		var x = radius + Math.random() * (width - 2 * radius);
+		var y = radius + Math.random() * (height - 2 * radius);
 		var dx = Math.random() - 0.5;
 		var dy = Math.random() - 0.5;
 		var color = '#' + parseInt(Math.random() * 0xFFFFFF).toString(16);
